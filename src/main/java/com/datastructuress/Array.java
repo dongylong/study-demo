8a64248dfe9d36349c6dab1863b52e461843e7a0
dongylong <dongyuanlong@live.cn>
2018-05-23 20:51:25 +0800

    singleton

--- src/main/java/com/datastructuress/Array.java
+++ src/main/java/com/datastructuress/Array.java
@@ -2,6 +2,10 @@ package com.datastructuress;
 
 import org.apache.commons.lang.ArrayUtils;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
 /**
  * @author dongyl
  * @date 18:37 5/7/18
@@ -18,5 +22,9 @@ public class Array {
         int[] a = null;
         System.out.println(ArrayUtils.isEmpty(a) +"...... isEmpty");
         System.out.println(a == null ? 0 : 1);
+    }
+    private void copyOnWriteArrayListDemo(){
+        List<Integer> array = new CopyOnWriteArrayList<>();
+        CopyOnWriteArraySet<Object> set = new CopyOnWriteArraySet<>();
     }
 }
