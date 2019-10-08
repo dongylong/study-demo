2501552f9a97ae39ad2961aee5fd9191147ceeb7
dongyl <dongyuanlong@live.cn>
2019-10-08 09:05:37 +0800

    add es demo

---
+++ src/main/java/com/study/es/Config.java
@@ -0,0 +1,21 @@
+package com.study.es;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.node.Node;
+
+import java.io.IOException;
+
+/**
+ * @author dongyl
+ * @version 1.0
+ * @title
+ * @description
+ * @company 好未来-爱智康
+ * @created 2019/10/1 22:23
+ * @changeRecord
+ */
+public class Config {
+    public static void main(String[] args) {
+    }
+}
